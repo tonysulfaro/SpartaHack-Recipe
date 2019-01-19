f3ea8208e1ee3b5d20c232b4a5e24bad02d03412
Tony Sulfaro <tony116523@gmail.com>
2019-01-19 03:02:39 -0500

    initial work

---
+++ script.js
@@ -0,0 +1,9 @@
+function getrecipie() {
+    let search_term = document.getElementById("search_box").value;
+    console.log(search_term);
+    generateRecipieResults(search_term);
+}
+
+function generateRecipieResults(search_term) {
+    console.log("generating view");
+}
